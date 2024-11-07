b7eb6f1d1c276d757eb4da763d0df4e40797d47f
minhtrang04 <140622296+minhtrang04@users.noreply.github.com>
2024-11-07 11:28:53 +0700

    Time: 648 ms (48.08%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -2,5 +2,5 @@
 select customer_id 
 from Customer inner join Product on
 Customer.product_key = Product.product_key
-GROUP BY 1
-HAVING COUNT(DISTINCT Product.product_key) = (SELECT COUNT(product_key) FROM Product);
+GROUP BY customer_id
+HAVING COUNT(DISTINCT Customer.product_key) = (SELECT COUNT(product_key) FROM Product);
